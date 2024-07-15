3f67a4055feb957ef8577e076e6b932e45035ac3
SaskaSolutions <saskasolutions@gmail.com>
2024-07-15 15:07:36 +0500

    check

--- frontend/src/components/Notification.jsx
+++ frontend/src/components/Notification.jsx
@@ -1,32 +1,32 @@
 import React from 'react';
 
 const Notification = () => {
-    const Nots = [
-        {
-            type: 'Low',
-            alert: 'Item 1 is low on stock',
-        },
-        {
-            type: 'Low',
-            alert: 'Item 2 is low on stock',
-        },
-        {
-            type: 'Due',
-            alert: 'Order 3 is OverDue',
-        },
-        {
-            type: 'Due',
-            alert: 'Order 4 is OverDue',
-        },
-        {
-            type: 'Del',
-            alert: 'Order 5 is Delivering Today',
-        }
-    ];
+    // const Nots = [
+    //     {
+    //         type: 'Low',
+    //         alert: 'Item 1 is low on stock',
+    //     },
+    //     {
+    //         type: 'Low',
+    //         alert: 'Item 2 is low on stock',
+    //     },
+    //     {
+    //         type: 'Due',
+    //         alert: 'Order 3 is OverDue',
+    //     },
+    //     {
+    //         type: 'Due',
+    //         alert: 'Order 4 is OverDue',
+    //     },
+    //     {
+    //         type: 'Del',
+    //         alert: 'Order 5 is Delivering Today',
+    //     }
+    // ];
     // Added A Comment
     return (
         <div className='mx-4 h-[40vh] w-[40vh] border-2 bg-white rounded z-10 ml-auto fixed right-0 top-15 p-2 overflow-auto'>
-            {Nots.length > 0 ? (
+            {/* {Nots.length > 0 ? (
                 Nots.map((notification, index) => (
                     <div key={index} className='w-100 h-auto p-2 bg-gray-100 my-2'>
                         <div className='flex'>
@@ -38,11 +38,11 @@ const Notification = () => {
                         </div>
                     </div>
                 ))
-            ) : (
-                <div className='w-100 h-auto p-2 bg-gray-200'>
-                    <p className='text-center'>No notifications</p>
-                </div>
-            )}
+            ) : ( */}
+            <div className='w-100 h-auto p-2 bg-gray-200'>
+                <p className='text-center'>No notifications</p>
+            </div>
+            {/* )} */}
         </div>
     );
 };
